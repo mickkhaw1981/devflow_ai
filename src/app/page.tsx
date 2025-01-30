d98650fab48faa9d75400a6872add097a133486f
Mike Khaw <mikekhawnow@gmail.com>
2025-01-30 13:21:12 -0800

    Style home page with centered, large text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
-      <h1>DevFlow AI</h1>
+      <h1 className="text-4xl font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">DevFlow AI</h1>
     </div>
   );
 }
